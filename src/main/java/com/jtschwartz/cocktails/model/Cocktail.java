0c6ffceb37b9fa357348910ba451dd8b395a4cef
JTSchwartz <jacob@jtschwartz.com>
2025-02-09 19:44:28 -0500

    Test corrections

--- src/main/java/com/jtschwartz/cocktails/model/Cocktail.java
+++ src/main/java/com/jtschwartz/cocktails/model/Cocktail.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Accessors(chain = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Document("${spring.data.mongodb.collection-prefix}-cocktails")
